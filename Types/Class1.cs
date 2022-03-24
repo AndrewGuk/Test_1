54ff1839fa821efb681c5b850daeb6313d67b1e7
Andrei Huk <belakolala1@gmail.com>
2022-03-24 21:52:31 +0300

    Добавьте файлы проекта.

---
+++ Types/Class1.cs
@@ -0,0 +1,74 @@
+﻿namespace Types
+{
+    //TODD 2
+    public class MyString
+    {
+        public string Value { get; set; } 
+
+        public MyString(string x)
+        {
+            this.Value = x;
+        }
+        public MyString(string x, string y)
+        {
+            this.Value = x + y;
+        }
+        public MyString(string x, char y)
+        {
+            this.Value=x + y;
+        }
+        public MyString(char x, char y)
+        {
+            this.Value = (x + y).ToString();
+        }
+        public MyString()
+        {
+        }
+
+        public string Revers(string x)
+        {
+            char[] arr = x.ToCharArray();
+            Array.Reverse(arr);
+            return new string(arr);
+
+        }
+    }
+    //TODD 5
+    public class Transport
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"Name: {Name}\nAge: {Age.ToString()}");
+            
+        }
+    }
+    public class Car : Transport
+    {
+        public string Type { get; set; }
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"Type: {Type}");
+        }
+    }
+    public class Boat : Transport
+    {
+        public decimal Lenght { get; set; }
+        public override void PrintInfo()
+        {
+            base.PrintInfo();
+            Console.WriteLine($"Lenght: {Lenght}");
+        }
+
+    }
+    static class Data
+    {
+        public static string Print(this DateTime dateTime)
+        {
+            
+        }
+    }
+}
